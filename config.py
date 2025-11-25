3b5704d20311ecc358400f3421f601307b107797
Smriti Gupta <147394905+Smriti-26@users.noreply.github.com>
2025-11-26 03:18:03 +0530

    Update GEMINI_API_KEY to use a secret key

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ CLEAN_TEXT_DIR = "./clean_prose_chapters"
 INDEX_DIR = "./rag_indices"
 
 # --- API AND MODEL CONFIGURATION ---
-GEMINI_API_KEY = "AIzaSyBmNjjOwzzfWW1sk9gfG8PSbu-UTngYZVY" 
+GEMINI_API_KEY = "my_secret_key" 
 
 # Model for summarization and final generation (Now a Chat Model)
 GENERATION_MODEL = "gemini-2.5-flash-preview-09-2025" 
@@ -23,4 +23,5 @@ if not os.path.exists(INDEX_DIR):
     os.makedirs(INDEX_DIR)
 
 if not GEMINI_API_KEY or GEMINI_API_KEY == "YOUR_GEMINI_API_KEY_HERE":
+
     print("🚨 WARNING: Please set your GEMINI_API_KEY in config.py")
